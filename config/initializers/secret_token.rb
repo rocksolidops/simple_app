5a5662651c424d100741e019ba3ccb301181b1fb
Mathieu Martin <webmat@gmail.com>
2013-06-06 14:01:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebt::Application.config.secret_token = '2c9bb89fa3be5f3f077c55d8fccaaba12da92bf889b6aa853b0e7f72ac49e19883a2b629105ae021148c58fe5568ed89cc14ea64675f42707caec013f7e7a59a'
